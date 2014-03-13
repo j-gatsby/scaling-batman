9f2c38b68b5b2624714e6d7263d93a50912a19ca
j-gatsby <timjoeckel@gmail.com>
2014-03-13 19:54:05 -0400

    Add ctype.h

--- caesar.c
+++ caesar.c
@@ -4,43 +4,44 @@
 #include <stdio.h>		// for standard input & output
 #include <string.h>		// for strlen
 #include <stdlib.h>		// for atoi
-
+#include <ctype.h>		// for isalpha
 #define ALPHA 26
 
 int main(int argc, string argv[])
 {
-	int n = argc;
-	int key = (int)atoi(argv[n - 1]);
-	if ((argc != 2) || (key < 0)) 
-	{
-		printf("You must enter a single, non-negative int to use as the key, after the program name!\n");
-		printf("ex.= jharvard $ ./caesar 13\n");
-		printf("Try again, buddy.\n");
-		return 1;
-	}
-	else 
-	{
-		string input = GetString();
-
-		for (int i = 0, n = strlen(input); i < n; i++)
+	int a = argc;
+	string k = argv[a - 1];
+	int key = (int)atoi(k);
+		if ((a != 2) || (key < 0)) 
 		{
-			if (input[i] >= 'a' && input[i] <= 'z') 
-			{
-				char after = (((input[i] - 'a' + key) % ALPHA) + 'a'); 
-				printf("%c", after);
-			}
-			else if (input[i] >= 'A'&& input[i] <= 'Z')
-			{
-				char after = (((input[i] - 'A' + key) % ALPHA) + 'A');
-				printf("%c", after);
-			}
-			else 
-			{
-				printf("%c", input[i]);
-			}
+			printf("You must enter a single, non-negative int to use as the key, after the program name!\n");
+			printf("ex.= jharvard $ ./caesar 13\n");
+			printf("Try again, buddy.\n");
+			return 1;
 		}
-	}	
-	printf("\n");
-	return 0;
+		else 
+		{
+			string input = GetString();
+
+			for (int i = 0, n = strlen(input); i < n; i++)
+			{
+				if (input[i] >= 'a' && input[i] <= 'z') 
+				{
+					char after = (((input[i] - 'a' + key) % ALPHA) + 'a'); 
+					printf("%c", after);
+				}
+				else if (input[i] >= 'A'&& input[i] <= 'Z')
+				{
+					char after = (((input[i] - 'A' + key) % ALPHA) + 'A');
+					printf("%c", after);
+				}
+				else 
+				{
+					printf("%c", input[i]);
+				}
+			}
+		}	
+		printf("\n");
+		return 0;
 }
 
