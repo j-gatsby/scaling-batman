8aa9ca2726ee2da836875839148a069e201f09e3
j-gatsby <timjoeckel@gmail.com>
2014-03-13 18:40:03 -0400

    Add caesar.c

---
+++ caesar.c
@@ -0,0 +1,45 @@
+// a program that encrypts messages using Caesar’s cipher
+#include <cs50.h>		// for GetString()
+#include <stdio.h>		// for input & output
+#include <string.h>		// for string
+#include <stdlib.h>		// for atoi
+
+#define ALPHA 26
+
+int main(int argc, string argv[])
+{
+	int n = argc;
+	int key = (int)atoi(argv[n - 1]);
+	if ((argc != 2) || (key < 0)) 
+	{
+		printf("You must enter a single, non-negative int to use as the key, after the program name!\n");
+		printf("ex.= jharvard $ ./caesar 13\n");
+		printf("Try again, buddy.\n");
+		return 1;
+	}
+	else 
+	{
+		string input = GetString();
+
+		for (int i = 0, n = strlen(input); i < n; i++)
+		{
+			if (input[i] >= 'a' && input[i] <= 'z') 
+			{
+				char after = (((input[i] - 'a' + key) % ALPHA) + 'a'); 
+				printf("%c", after);
+			}
+			else if (input[i] >= 'A'&& input[i] <= 'Z')
+			{
+				char after = (((input[i] - 'A' + key) % ALPHA) + 'A');
+				printf("%c", after);
+			}
+			else 
+			{
+				printf("%c", input[i]);
+			}
+		}
+	}	
+	printf("\n");
+	return 0;
+}
+
